43aef3bdad10771ac13fcffbc50b4929347b5cce
mbfreitas0 <mbfreitas0@gmail.com>
2022-08-21 16:58:03 -0300

    commit-21082022

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,34 @@
-import { Link } from "react-router-dom";
-//import { BicameraMovie, BiSearchAlt2 } from "react-icons/bi";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { BiCameraMovie, BiSearchAlt2 } from "react-icons/bi";
+import './Navbar.css';
 
-const Navbar = () => {
-  return (
-    <nav id="navbar">
-      <h2>
-        <Link to="/">MoviesLib</Link>
-      </h2>
-      <form action="">
-        <input type="text" placeholder="Busque um fime" />
-        <button type="submit"></button>
-      </form>
-    </nav>
-  );
-};
-export default Navbar;
 
+  const Navbar = () => {
+    const [search, setSearch] = useState("");
+
+    const navigate = useNavigate();
+
+    const handleSubmit = (e) => {
+      e.preventDefault();
+
+      if (!search) return;
+
+      navigate(`/search?q=${ search }`, { replace: true });
+      setSearch("");
+    };
+
+    return (
+      <nav id="navbar">
+        <h2>
+          <Link to="/"><BiCameraMovie />MoviesLib</Link>
+        </h2>
+        <form onSubmit={handleSubmit}>
+          <input type="text" placeholder="Busque um fime" onChange={(e) =>
+            setSearch(e.target.value)} value={search} />
+          <button type="submit"><BiSearchAlt2 /></button>
+        </form>
+      </nav>
+    );
+  };
+  export default Navbar
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,6 @@
 import { useState, useEffect } from "react";
+import MovieCard from "../components/MovieCard";
+import './MoviesGrid.css';
 
 const moviesURL = import.meta.env.VITE_API
 const apiKey = import.meta.env.VITE_API_KEY;
@@ -11,7 +13,7 @@ const Home = () => {
         const res = await fetch(url);
         const data = await res.json();
 
-        setTopMovies.log(data.results);
+        setTopMovies(data.results);
     };
 
 useEffect(() => {
@@ -26,10 +28,8 @@ useEffect(() => {
             <h2 className="title">Melhores Filmes :</h2>
             <div className="movies-container">
             { topMovies === 0 && <p>Carregando ...</p> }    
-            { topMovies.length > 0 && topMovies.map((movie) => {
-                
-                <p>movie.title</p>
-            })}
+            { topMovies.length > 0 && topMovies.map((movie) => <MovieCard key={ movie.id } movie={ movie }/>
+            )}
             </div>
         </div>
         
